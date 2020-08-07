0aeb15620b925ffee3f1512fe01cacbaa165994b
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-08-07 21:40:17 +0100

    Partial commit - styled components

---
+++ git-watch/src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+    return <Title>Explore repositories on Github</Title>;
+}
+
+export default Dashboard;
