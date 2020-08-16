c36cd1cfa7305296426c6fd691ef4c057f61961e
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-08-16 11:24:42 +0100

    Main page layout partial commit

--- git-watch/src/pages/Dashboard/index.tsx
+++ git-watch/src/pages/Dashboard/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositories on Github</Title>;
+    return(
+        <>
+            <img src={logoImg} alt="GitWatch"/>
+            <Title>Explore repositories on Github</Title>
+
+            <Form>
+                <input placeholder="type repository's name"/>
+                <button>Search</button>
+            </Form>
+
+            <Repositories>
+                <a href="test">
+                    <img src="https://avatars3.githubusercontent.com/u/11893246?s=460&u=885f65e47c64046d27c22eec9f9b66f65f277477&v=4" alt="Tiago Vaccari"/>
+                </a>
+            </Repositories>
+        </>
+    )
 }
 
 export default Dashboard;
