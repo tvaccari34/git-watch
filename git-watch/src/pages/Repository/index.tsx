40d6305ae0c6ace683c4227c7715a648f7f26fbe
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-08-27 18:15:49 +0100

    Added api calls for Repositories

--- git-watch/src/pages/Repository/index.tsx
+++ git-watch/src/pages/Repository/index.tsx
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import { Header, RepositoryInfo, Issues } from './styles';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+import api from '../../services/api';
 
 interface RepositoryParams {
     repository: string;
 }
 
+interface Repository {
+    full_name: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+    description: string;
+    stargazers_count: number;
+    forks_count: number;
+    open_issues_count: number;
+}
+
+interface Issue {
+    id: string;
+    title: string;
+    user: {
+        login: string;
+    }
+}
+
+
 const Repository: React.FC = () => {
 
+    const [ repository, setRepository ] = useState<Repository | null>(null);
+    const [ issues, setIssues ] = useState<Issue[]>([]);
+
     const { params } = useRouteMatch<RepositoryParams>();
+
+    useEffect(() => {
+
+        api.get(`repos/${params.repository}`).then(response => {
+            setRepository(response.data);
+        });
+
+        api.get(`repos/${params.repository}/issues`).then(response => {
+            setIssues(response.data);
+        });
+
+        // async function loadData(): Promise<Repository> {
+        //     const [ repository, issues ] = await Promise.all([
+        //         api.get(),
+        //         api.get(`repos/${params.repository}/issues`)
+        //     ]);
+
+
+        //     return repository;
+        // }
+
+        //loadData();
+    }, [params.repository]);
 
     return (
         <>
@@ -22,34 +70,36 @@ const Repository: React.FC = () => {
                 </Link>
             </Header>
 
-            <RepositoryInfo>
-                <header>
-                    <img src="https://avatars0.githubusercontent.com/u/11893246?v=4" alt="tvaccari34"/>
-                    <div>
-                        <strong>tvaccari34/Ecollect</strong>
-                        <p>Descricao do repositorio</p>
-                    </div>
-                </header>
-                <ul>
-                    <li>
-                        <strong>1808</strong>
-                        <span>Stars</span>
-                    </li>
-                    <li>
-                        <strong>48</strong>
-                        <span>Forks</span>
-                    </li>
-                    <li>
-                        <strong>67</strong>
-                        <span>Issues</span>
-                    </li>
-                </ul>
-            </RepositoryInfo>
+            {repository && (
+                <RepositoryInfo>
+                    <header>
+                        <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                    </header>
+                    <ul>
+                        <li>
+                            <strong>{repository.stargazers_count}</strong>
+                            <span>Stars</span>
+                        </li>
+                        <li>
+                            <strong>{repository.forks_count}</strong>
+                            <span>Forks</span>
+                        </li>
+                        <li>
+                            <strong>{repository.open_issues_count}</strong>
+                            <span>Issues</span>
+                        </li>
+                    </ul>
+                </RepositoryInfo>
+            )}
 
             <Issues>
                 <Link to={"dfadfadsfds"}>
                     <div>
-                        <strong>asdfasdfadf</strong>
+                        <strong></strong>
                         <p>sdfasdfasdf</p>
                     </div>
                     <FiChevronRight size={20} />
